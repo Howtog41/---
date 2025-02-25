67114dd4e2a8129390af61ff15a9936c51ec76a6
Howtog41 <howtog41@gmail.com>
2025-02-25 06:02:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ BOT_TOKEN = "8151017957:AAF15t0POw7oHaFjC-AySwvDmNyS3tZxbTI"
 
 MONGO_URI = "mongodb+srv://terabox255:h9PjRSpCHsHw5zzt@cluster0.nakwhlt.mongodb.net/?retryWrites=true&w=majority"
 DB_NAME = "quiz_bot_db"
-QUIZ_COLLECTION = "quizzes"
-RANK_COLLECTION = "rankings"
+quiz_collection = "quizzes"
+rank_collection = "rankings"
