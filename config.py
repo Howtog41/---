0a99fc4cd7693e2113ba1925df1fa316ed25598b
Howtog41 <howtog41@gmail.com>
2025-02-25 05:52:30 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+BOT_TOKEN = "YOUR_BOT_TOKEN"
+
+MONGO_URI = "mongodb+srv://terabox255:h9PjRSpCHsHw5zzt@cluster0.nakwhlt.mongodb.net/?retryWrites=true&w=majority"
+DB_NAME = "quiz_bot_db"
+QUIZ_COLLECTION = "quizzes"
+RANK_COLLECTION = "rankings"
