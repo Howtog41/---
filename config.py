fc26eea9c60b56098c7492a824aae5f75ceb971e
Howtog41 <howtog41@gmail.com>
2025-02-25 05:57:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-BOT_TOKEN = "YOUR_BOT_TOKEN"
+BOT_TOKEN = "8151017957:AAF15t0POw7oHaFjC-AySwvDmNyS3tZxbTI"
 
 MONGO_URI = "mongodb+srv://terabox255:h9PjRSpCHsHw5zzt@cluster0.nakwhlt.mongodb.net/?retryWrites=true&w=majority"
 DB_NAME = "quiz_bot_db"
