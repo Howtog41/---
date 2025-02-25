8ad66bcf2a1ed5feea66acedf0f866a0ea0fac52
Howtog41 <howtog41@gmail.com>
2025-02-25 18:33:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import requests
 import re
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pymongo import MongoClient
-
+from plugins import rank   
 # ✅ MongoDB Connection
 MONGO_URI = "mongodb+srv://terabox255:h9PjRSpCHsHw5zzt@cluster0.nakwhlt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 client = MongoClient(MONGO_URI)
