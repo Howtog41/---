d54fafc99f3ff078b344a9b4ac864d831b9d4de8
Howtog41 <howtog41@gmail.com>
2026-02-09 13:26:23 +0530

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,5 +1,8 @@
+
+# scheduler.py
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from pytz import timezone
 
-scheduler = AsyncIOScheduler(timezone=timezone("Asia/Kolkata"))
-scheduler.start()
+scheduler = AsyncIOScheduler(
+    timezone=timezone("Asia/Kolkata")
+)
